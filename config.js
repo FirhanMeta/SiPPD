b9fc80683b7b12949a443cde230ee511c46da7f3
FirhanMeta <firhanmeta@gmail.com>
2026-05-11 10:25:32 +0800

    add config

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+const SUPA_URL   = 'https://akttsoaukobrvxwgflfs.supabase.co';
+const SUPA_KEY   = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrdHRzb2F1a29icnZ4d2dmbGZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzOTA1MzksImV4cCI6MjA5Mzk2NjUzOX0.179azq7t0qhesmR6J5mWxlH5Xrn9v3h8xSQo33s1zJM';
+const SUPA_TABLE = 'attendance';
